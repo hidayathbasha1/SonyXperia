0629787ffbb4a7585f2e05a861b759ab708c0938
hidayathbasha1 <hidayathbasha4@gmail.com>
2019-02-22 12:28:27 +0530

    code change

--- src/main/java/jar/App.java
+++ src/main/java/jar/App.java
@@ -1,13 +1,6 @@
-package jar;
-
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
+ class HelloWorld { 
+ public static void main(String[] args) 
+ { 
+ System.out.println("Hello World!"); 
+ } 
+} 
